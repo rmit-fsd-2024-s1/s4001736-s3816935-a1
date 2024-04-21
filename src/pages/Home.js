cfadd1a4f4c28c5630e87adb4aa71ae323348cb5
Jewel2323 <sereyrothna.sok@gmail.com>
2024-04-21 12:53:50 +1000

    Start of small scale farming

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,9 +39,26 @@ export default function Home(props) {
       <div class="homepage-headings">
       <h2 >Small-Scale Farming</h2>
       </div>
-       <div class="homepage-sections">
+
+      <div class="smallScaleFarming">
         <h4>Want to grow your own veggies?</h4>
-       </div>
+
+        <br></br>
+
+        <p>No fear! SOIL is ready to guide you in simple points on how you can get started with your own mini garden!</p>
+
+        <br></br>
+
+        <p>Just consider these 5 points:</p>
+        <ul>
+          <li>Size of your garden</li>
+          <li>Size of you plant pots</li>
+          <li>Choosing your plants and vegetables</li>
+          <li>When to plant</li>
+          <li>Planning ahead</li>
+        </ul>
+        
+      </div>
 
     </div>
     
