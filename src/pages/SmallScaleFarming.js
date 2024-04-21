76c7e96d5276ac1363ff591d56ca5639c56e33e3
Jewel2323 <sereyrothna.sok@gmail.com>
2024-04-21 19:05:03 +1000

    Small Scale Farming imgs

--- src/pages/SmallScaleFarming.js
+++ src/pages/SmallScaleFarming.js
@@ -7,12 +7,12 @@ const smallScaleFarming =
 
         <p>No fear! SOIL is ready to guide you in simple points on how you can get started with your own mini garden!</p>
 
-        <br></br>
+        <img src="/src/images/homegarden.jpeg"></img>
 
         <p>Just consider these 5 points:</p>
         <ul>
+          <li>Picking the right spot</li>
           <li>Size of your garden</li>
-          <li>Size of you plant pots</li>
           <li>Choosing your plants and vegetables</li>
           <li>When to plant</li>
           <li>Planning ahead</li>
@@ -20,23 +20,49 @@ const smallScaleFarming =
 
         <br></br>
 
-        <h5>Garden size</h5>
-        <p>Knowing how big your garden or space for planting your favourite vegetables is important as different veggies grow in different sizes and as well knowing how much can you grow. Plus, it also helps you be considerate about choosing what vegetables you are able to grow.</p>
+        <h5>Choosing a home for your plants</h5>
+        <p>Plants are like little babies, you would need time, dedication and resources to feed and care for them. It's important for them to have good exposure to sunlight, is hydrated well enough, has stable support upon strong winds, and a nutrient rich soil.</p>
 
         <br></br>
-        
-        <h5>Plant Pot size</h5>
+
+        <h5>Garden Size</h5>
+        <p>Knowing how big your garden or space for planting your favourite vegetables is important as different veggies grow in different sizes and as well knowing how much can you grow. Plus, it also helps you be considerate about choosing what vegetables you are able to grow.</p>
         <p>Once you know how big your gardening space is, you can now easily pick which plant pots size you would need. Many plants would need to be place in pots first before being able to be transfer onto the soil.</p>
 
         <br></br>
 
         <h5>Choosing Vegetables</h5>
-        <p>It's importantant to understand what vegetables you can or can't plant within your environment. Australian is a big land where climate differs from one region to another. Sydney has a subtropical climate while Melbourne has a cooler temperate climate. It's best to consider the climate factors of what veggies are suitable for each climate.</p>
+        <p>It's important to understand what vegetables you can or can't plant within your environment. Australia is a giant body of land where climate differs from one region to another. Sydney has a subtropical climate while Melbourne has a cooler temperate climate. It's best to consider the climate factors of what veggies are suitable for each climate, and especially for the region you are located in.</p>
+
+        <br></br>
+
+        <p>Besides, here is a list of some easy veggies to grow here in Australia:</p>
+        
+        <ul>
+          <li>Leafy greens</li>
+          <li>Spring onions</li>
+          <li>Tomatoes</li>
+          <li>Beetroot</li>
+          <li>Radish</li>
+          <li>Beans</li>
+          <li>Cucumbers</li>
+          <li>Silverbeets</li>
+          <li>Zucchini</li>
+        </ul>
+
+        <p>If you are still unsure what to grow, it's best to consider what are your favourite veggies or the household's favourite. This home farm can help sustain some costs too.</p>
 
         <br></br>
 
         <h5>When to Plant</h5>
         <p>Similarly, the timing of when to plant some vegetables are also important. With different climate zones also comes with different seasons that would cycle through. Different seasons call for different characteristics of weather and temperature as well where certain plants can or can't grow.</p>
+          
+        <p>Although the easy veggies to grow listed above as they suit to most seasons, sometimes it is best to grow them in their respective suitable seasons such as leafy greens in late summer where they will be able to grow better in the cooler seasons of autumn and winter.</p>
+
+        <br></br>
+
+        <h5>Planning Ahead</h5>
+        <p>And that's about it! You are now ready to plan ahead and get started with your mini farming journey.</p>
 
       </div>
       ;
