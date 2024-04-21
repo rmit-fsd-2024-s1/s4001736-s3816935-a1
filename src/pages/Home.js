6d9d83285656c09d8af4776dc40f584941e37789
Jewel2323 <sereyrothna.sok@gmail.com>
2024-04-21 12:29:23 +1000

    Flow of products arranged

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,23 +10,40 @@ export default function Home(props) {
   return (
     <div>
       {/* {props.username !== null && <h4><strong>Hello {props.username}!</strong></h4>} */}
+      <div class="homepage-headings">
       <h2>Weekly Specials</h2>
+      </div>
+
+      <div class="homepage-sections">
       {products.map((item, index) => {
         // console.log(index)
         if (index < 4) {
           return <StoreItems id={item.productID} name={item.productName} price={item.productPrice} image={item.productImage} username={props.username}/>
         }
       })}
+      </div>
+
+      <div class="homepage-headings">
       <h2>Organic Products</h2>
+      </div>
+
+      <div class="homepage-sections">
       {products.map((item, index) => {
         // console.log(index)
         if (index > 3) {
           return <StoreItems id={item.productID} name={item.productName} price={item.productPrice} image={item.productImage} username={props.username}/>
         }
       })}
-      <br />
-      <h2>Small-Scale Farming</h2>
-      <p> </p>
+      </div>
+      
+      <div class="homepage-headings">
+      <h2 >Small-Scale Farming</h2>
+      </div>
+       <div class="homepage-sections">
+        <p> </p>
+       </div>
+
     </div>
+    
   );
 }
