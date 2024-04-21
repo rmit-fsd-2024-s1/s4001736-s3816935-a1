fea266750de8004077863ded83aacb6bef0a2b05
Jewel2323 <sereyrothna.sok@gmail.com>
2024-04-21 15:20:38 +1000

    Small Scale Farming moved into own seperate file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getProducts } from "../repository/products";
 import StoreItems from "./StoreItem";
+import smallScaleFarming from "./SmallScaleFarming";
 
 export default function Home(props) {
   const products = getProducts();
@@ -40,37 +41,10 @@ export default function Home(props) {
       <h2 >Small-Scale Farming</h2>
       </div>
 
-      <div class="smallScaleFarming">
-        <h4>Want to grow your own veggies?</h4>
-
-        <br></br>
-
-        <p>No fear! SOIL is ready to guide you in simple points on how you can get started with your own mini garden!</p>
-
-        <br></br>
-
-        <p>Just consider these 5 points:</p>
-        <ul>
-          <li>Size of your garden</li>
-          <li>Size of you plant pots</li>
-          <li>Choosing your plants and vegetables</li>
-          <li>When to plant</li>
-          <li>Planning ahead</li>
-        </ul>
-
-        <br></br>
-
-        <h5>Garden size</h5>
-        <p>Knowing how big your garden or space for planting your favourite vegetables is important as different veggies grow in different sizes and as well knowing how much can you grow. Plus, it also helps you be considerate about choosing what vegetables you are able to grow.</p>
-
-        <br></br>
-        
-        <h5>Plant Pot size</h5>
-        <p>Once you know how big your gardening space is, you can now easily pick which plant pots size you would need. Many plants would need to be place in pots first before being able to be transfer onto the soil.</p>
-
-      </div>
+      {smallScaleFarming}
 
     </div>
     
   );
 }
+
---
+++ src/pages/SmallScaleFarming.js
@@ -0,0 +1,37 @@
+const smallScaleFarming = 
+
+    <div class="smallScaleFarming">
+        <h4>Want to grow your own veggies?</h4>
+
+        <br></br>
+
+        <p>No fear! SOIL is ready to guide you in simple points on how you can get started with your own mini garden!</p>
+
+        <br></br>
+
+        <p>Just consider these 5 points:</p>
+        <ul>
+          <li>Size of your garden</li>
+          <li>Size of you plant pots</li>
+          <li>Choosing your plants and vegetables</li>
+          <li>When to plant</li>
+          <li>Planning ahead</li>
+        </ul>
+
+        <br></br>
+
+        <h5>Garden size</h5>
+        <p>Knowing how big your garden or space for planting your favourite vegetables is important as different veggies grow in different sizes and as well knowing how much can you grow. Plus, it also helps you be considerate about choosing what vegetables you are able to grow.</p>
+
+        <br></br>
+        
+        <h5>Plant Pot size</h5>
+        <p>Once you know how big your gardening space is, you can now easily pick which plant pots size you would need. Many plants would need to be place in pots first before being able to be transfer onto the soil.</p>
+
+        <h5>Choosing Vegetables</h5>
+        <p>It's importantant to understand what vegetables you can or can't plant </p>
+
+      </div>
+      ;
+
+      export default smallScaleFarming;
