ca0a75b4a89815e2adb266d8ee587f5c31da3805
Jewel2323 <sereyrothna.sok@gmail.com>
2024-04-21 19:24:17 +1000

    Finalising small scale farming

--- src/pages/SmallScaleFarming.js
+++ src/pages/SmallScaleFarming.js
@@ -1,3 +1,10 @@
+import img1 from "../images/homegarden.jpeg";
+import img2 from "../images/plantneeds.jpeg";
+import img3 from "../images/gardensize.jpeg";
+import img4 from "../images/varietyvegetables.jpeg";
+import img5 from "../images/seasons.jpeg";
+import img6 from "../images/gardeningplanning.jpeg";
+
 const smallScaleFarming = 
 
     <div class="smallScaleFarming">
@@ -7,7 +14,9 @@ const smallScaleFarming =
 
         <p>No fear! SOIL is ready to guide you in simple points on how you can get started with your own mini garden!</p>
 
-        <img src="/src/images/homegarden.jpeg"></img>
+        <img className="ssfImg" src={img1}></img>
+
+        <br></br>
 
         <p>Just consider these 5 points:</p>
         <ul>
@@ -21,20 +30,25 @@ const smallScaleFarming =
         <br></br>
 
         <h5>Choosing a home for your plants</h5>
+        <img className="ssfImg" src={img2}></img>
         <p>Plants are like little babies, you would need time, dedication and resources to feed and care for them. It's important for them to have good exposure to sunlight, is hydrated well enough, has stable support upon strong winds, and a nutrient rich soil.</p>
 
         <br></br>
 
         <h5>Garden Size</h5>
+        <img className="ssfImg" src={img3}></img>
         <p>Knowing how big your garden or space for planting your favourite vegetables is important as different veggies grow in different sizes and as well knowing how much can you grow. Plus, it also helps you be considerate about choosing what vegetables you are able to grow.</p>
         <p>Once you know how big your gardening space is, you can now easily pick which plant pots size you would need. Many plants would need to be place in pots first before being able to be transfer onto the soil.</p>
 
         <br></br>
 
         <h5>Choosing Vegetables</h5>
+        
         <p>It's important to understand what vegetables you can or can't plant within your environment. Australia is a giant body of land where climate differs from one region to another. Sydney has a subtropical climate while Melbourne has a cooler temperate climate. It's best to consider the climate factors of what veggies are suitable for each climate, and especially for the region you are located in.</p>
 
         <br></br>
+
+        <img className="ssfImg" src={img4}></img>
 
         <p>Besides, here is a list of some easy veggies to grow here in Australia:</p>
         
@@ -55,6 +69,7 @@ const smallScaleFarming =
         <br></br>
 
         <h5>When to Plant</h5>
+        <img className="ssfImg" src={img5}></img>
         <p>Similarly, the timing of when to plant some vegetables are also important. With different climate zones also comes with different seasons that would cycle through. Different seasons call for different characteristics of weather and temperature as well where certain plants can or can't grow.</p>
           
         <p>Although the easy veggies to grow listed above as they suit to most seasons, sometimes it is best to grow them in their respective suitable seasons such as leafy greens in late summer where they will be able to grow better in the cooler seasons of autumn and winter.</p>
@@ -62,6 +77,7 @@ const smallScaleFarming =
         <br></br>
 
         <h5>Planning Ahead</h5>
+        <img className="ssfImg" src={img6}></img>
         <p>And that's about it! You are now ready to plan ahead and get started with your mini farming journey.</p>
 
       </div>
